a1d5ce68f7089354243f87bc60620f9a08c785ae
matbotW <mathewmcdade@gmail.com>
2020-01-06 03:14:53 -0800

    added simple use of package:english_words

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MatsApp());
 
@@ -13,14 +14,18 @@ class MatsApp extends StatelessWidget {
   //BUILD
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+
     return MaterialApp(
       title: "Welcome to Flutter",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Welcome to Flutter!"),
-        ),
-        body: Center(
-          child: Text("Hello World!"),
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Welcome to Flutter!"),
+          ),
+          body: Center(
+            child: Text(wordPair.asPascalCase),
+          ),
         ),
       ),
     );
