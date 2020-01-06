32e1eacaf424a2d40d20a8701305c8b4ccf2643b
matbotW <mathewmcdade@gmail.com>
2020-01-06 03:52:29 -0800

    implemented infinite scrolling list view of random word pairs

--- lib/main.dart
+++ lib/main.dart
@@ -7,26 +7,61 @@ import 'package:english_words/english_words.dart';
 void main() => runApp(MatsApp());
 
 class MatsApp extends StatelessWidget {
+  //BUILD
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Welcome to Flutter!",
+      home: RandomWords(),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
   //PROPERTIES
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18);
 
   //METHODS
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return Divider(); //Divider() is a 1px height divider
+        final index = i ~/ 2; //~/2 is integer division in Dart
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs()
+              .take(10)); //WordPairs are generated 10 at a time.
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
 
   //BUILD
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-
-    return MaterialApp(
-      title: "Welcome to Flutter",
-      home: SafeArea(
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text("Welcome to Flutter!"),
-          ),
-          body: Center(
-            child: Text(wordPair.asPascalCase),
-          ),
+    //I like the SafeArea widget because impinging on the system bar bothers me.
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Startup Name Generator"),
         ),
+        body: _buildSuggestions(),
       ),
     );
   }
